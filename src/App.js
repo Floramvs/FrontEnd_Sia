086ee4a00d1aeb00244cc22c029504db1d64af93
Floramvs <youlmi2005@gmail.com>
2025-04-23 01:02:31 +0200

    Initial commit - frontend SIA

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-// src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Dashboard from './components/Dashboard';
 import Clips from './components/Clips';
 import Config from './components/Config';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'; // <-- Import Bootstrap CSS
-// ... le reste de tes imports
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <div className="d-flex flex-column min-vh-100">
         <Header />
         <main className="flex-grow-1">
@@ -22,12 +19,14 @@ function App() {
               <Route path="/" element={<Dashboard />} />
               <Route path="/clips" element={<Clips />} />
               <Route path="/config" element={<Config />} />
+              {/* Fallback pour les routes non définies */}
+              <Route path="*" element={<p>Page non trouvée</p>} />
             </Routes>
           </div>
         </main>
         <Footer />
       </div>
-    </Router>
+    </BrowserRouter>
   );
 }
 
